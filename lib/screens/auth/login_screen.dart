8e95ab3acd769c2f8eece194bffd6960f94907c8
mauricioc21 <mauricioc21@github.com>
2025-11-13 15:59:49 +0000

    FIX: Agregar import faltante kDebugMode en login_screen

--- lib/screens/auth/login_screen.dart
+++ lib/screens/auth/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:provider/provider.dart';
 import 'package:image_picker/image_picker.dart';
 import '../../services/auth_service.dart';
